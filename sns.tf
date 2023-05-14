0edf6013c633a21c24f8b898be0d283a492f8d0a
ali <baitrantp@gmail.com>
2023-05-14 11:48:06 +0300

    added documentation

--- sns.tf
+++ sns.tf
@@ -3,8 +3,8 @@ resource "aws_sns_topic" "alarm_sns" {
 }
 
 resource "aws_sns_topic_subscription" "alarm_target" {
-  for_each  = toset(local.sns_emails)
+  count=   length(local.sns_emails)
   topic_arn = aws_sns_topic.alarm_sns.arn
   protocol  = "email"
-  endpoint  = each.value
+  endpoint  = local.sns_emails[count.index] 
 }
